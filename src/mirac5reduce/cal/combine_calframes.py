c0457958f4cf0ac753a88eb616662f9bfb264020
Taylor Tobin <31015670+tltobin@users.noreply.github.com>
2023-05-26 15:09:00 -0400

    Splitting off utils.write_mean_frame function
    Split off the part of code in cal.combine_calframes.combine_darks that wrote the calculated mean frame to a fits file and moved it to its own function, utils.utils.write_mean_frame.

--- src/mirac5reduce/cal/combine_calframes.py
+++ src/mirac5reduce/cal/combine_calframes.py
@@ -1,11 +1,10 @@
 ################## Importing packages ####################
 
 import os
-from astropy.io import fits
 import numpy as np
 import configparser
 
-from ..utils.utils import get_raw_filenames
+from ..utils.utils import get_raw_filenames, write_mean_frame
 from ..utils.memory_saver import lm_mean_frame
 
 ################## Functions ####################
@@ -143,44 +142,20 @@ def combine_darks( config, logfile = None, debug = False ):
     
     
     # Generates name of fits file results will be saved to
-    outfile_name = 'dark_{0}_{1}.fits'.format( raw_dark_startno, raw_dark_endno )
+    outfile_name = os.path.join( calib_outpath, 'dark_{0}_{1}.fits'.format( raw_dark_startno, raw_dark_endno ) )
     
     # Writes quick note to logfile or terminal
-    feedback_msg = 'COMBINE_DARKS:       Saving Results to {0}.'.format( os.path.join( calib_outpath, outfile_name ) )
+    feedback_msg = 'COMBINE_DARKS:       Saving Results to {0}.'.format( outfile_name )
     if logfile is not None:
         with open(logfile,'a') as lf:
             lf.write( '{0}\n'.format(feedback_msg) )
     else:
         print(feedback_msg)
-        
     
-    # Creates hdu to save to file with mean frame
-    hdu = fits.PrimaryHDU( avgframe )
     
-    # Populates header directly with general info 
-    hdu.header['FILETYPE'] =   'Combined Dark'
-    hdu.header['NFRAMES' ] = ( len(filelist), 'Number raw frames used' )
-    hdu.header['FILE_STR'] = ( filelist[0]  , 'First raw file used' )
-    hdu.header['FILE_END'] = ( filelist[-1] , 'Last raw file used' )
-    hdu.header['COMBTYPE'] = ( 'MEAN'       , 'How raw frames were combined' )
+    # Uses write_mean_frame function to save the calculated data to the desired fits file
+    write_mean_frame( outfile_name, avgframe, 'dark', filelist, raw_filepath = raw_cals_path )
     
-    # List of header keys to copy over from the first raw dark file
-    keys_to_copy = [ 'DATE', 'TIMEDAY', 'PLUS',                         # when first dark frame was taken
-                     'SNAP_VER', 'SNAPDATE', 'DEVICE', 'PARTNUM',       # versioning, if ever wanted
-                     'DCFILE', 'INITFILE',                              # ref files of potential interest
-                     'WINTRANS', 'DETPITCH', 'APERDIST', 'APERDIAM',    # some info about exposures, if wanted
-                     'FRMRATE', 'INTEGRT', 'INTEGRTM',                  # frame rate and integration
-                     'GAIN_SET', 'CH0POWER', 'CH1POWER', 'CH2POWER', 'CH3POWER', 'CH4POWER', 'CH5POWER' ]
-    
-    # Opens the first raw dark file used to create the mean and copies some values from its header to the
-    #   new header. Assumes these are in the 0th extension, not the data ext
-    with fits.open( os.path.join( raw_cals_path, filelist[0] ) ) as raw_ref_hdu:
-        for key in keys_to_copy:
-            if key in raw_ref_hdu[0].header.keys():
-                hdu.header[key] = raw_ref_hdu[0].header.cards[key][1:]
-    
-    # Finally, write this hdu to the output file
-    hdu.writeto( os.path.join( calib_outpath, outfile_name ) )
     
     
     
--- src/mirac5reduce/utils/utils.py
+++ src/mirac5reduce/utils/utils.py
@@ -2,6 +2,7 @@
 
 from glob import glob
 import os
+from astropy.io import fits
 
 
 ################## Functions ####################
@@ -92,4 +93,92 @@ def get_raw_filenames( raw_name_fmt, startno, endno, raw_file_path  ):
     
     # Returns list of files that were found
     return filelist
+
+
+def write_mean_frame( meanfile_name, avgframe, frametype, raw_filelist, raw_filepath = None ):
+    """
+    Saves mean frame calculated from a list of raw frames to an output fits file, populating the header with
+    some calculation details and some keys copied over from the first raw fits file used to calculate it.
     
+    Required Parameters
+    -------------------
+    
+            meanfile_name   String
+            
+                                The file name(s) (with paths) to which the output fits file with the mean
+                                frame and associated data will be saved.
+                                
+            avgframe        NumPy Array
+                            
+                                The mean frame calculated from the provided input frames. Will be saved in the
+                                output fits file's 0th extension.
+            
+            frametype       String: 'Dark', 'Flat', 'Obs', etc.
+                                
+                                Image type (i.e. dark, flat, obs) of the data frames averaged to create the 
+                                avgframe. Used to set the 'FILETYPE' fits header key.
+            
+            raw_filelist    List of Strings
+                            
+                                Sorted list of file names used to calculate the avgframe provided.
+                                
+                                
+                            
+    Optional Parameters
+    -------------------
+            
+            raw_filepath    String or None
+                                
+                                [ Default = None ]
+                            
+                                The path where the raw fits files in raw_filelist are stored. 
+                                
+                                If provided (not None), will copy over a number of header key cards from the
+                                first fits file in raw_filelist into the new output file.
+
+                            
+    Output Files Generated
+    ----------------------
+    
+        [meanfile_name]
+        
+                            Fits file containing (in extension 0) the provided avgframe as data. 
+                            
+                            Copies some info from original fits file headers to the extension 0 header of this
+                            file, as well as saving the start and end file numbers and the total number of
+                            files used.
+    
+    """
+    
+    
+    
+    # Creates hdu to save to file with mean frame
+    hdu = fits.PrimaryHDU( avgframe )
+    
+    # Populates header directly with general info 
+    hdu.header['FILETYPE'] =   'Combined {0}'.format( frametype.capitalize() )
+    hdu.header['NFRAMES' ] = ( len(raw_filelist), 'Number raw frames used' )
+    hdu.header['FILE_STR'] = ( raw_filelist[0]  , 'First raw file used' )
+    hdu.header['FILE_END'] = ( raw_filelist[-1] , 'Last raw file used' )
+    hdu.header['COMBTYPE'] = ( 'MEAN'       , 'How raw frames were combined' )
+    
+    # If a raw_filepath was provided, checks first file for desired keys and copies any to header of output
+    if raw_filepath is not None:
+        
+        # List of header keys to copy over from the first raw dark file
+        keys_to_copy = [ 'DATE', 'TIMEDAY', 'PLUS',                         # when first dark frame was taken
+                         'SNAP_VER', 'SNAPDATE', 'DEVICE', 'PARTNUM',       # versioning, if ever wanted
+                         'DCFILE', 'INITFILE',                              # ref files of potential interest
+                         'WINTRANS', 'DETPITCH', 'APERDIST', 'APERDIAM',    # some info about exposures, if wanted
+                         'FRMRATE', 'INTEGRT', 'INTEGRTM',                  # frame rate and integration
+                         'GAIN_SET', 'CH0POWER', 'CH1POWER', 'CH2POWER', 'CH3POWER', 'CH4POWER', 'CH5POWER' ]
+
+        # Opens the first raw dark file used to create the mean and copies some values from its header to the
+        #   new header. Assumes these are in the 0th extension, not the data ext
+        with fits.open( os.path.join( raw_filepath, raw_filelist[0] ) ) as raw_ref_hdu:
+            for key in keys_to_copy:
+                if key in raw_ref_hdu[0].header.keys():
+                    hdu.header[key] = raw_ref_hdu[0].header.cards[key][1:]
+    
+    # Finally, write this hdu to the output file
+    hdu.writeto( meanfile_name )
